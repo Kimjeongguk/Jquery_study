5e2ccb6ae1532e5f9c2fb5610f51bf1c891d2162
Kim Jeongguk <mindstar7949@naver.com>
2021-07-02 18:18:07 +0900

    pizzaHut_jquery

--- 6day/pizzaHut/js/scritp.js
+++ 6day/pizzaHut/js/scritp.js
@@ -16,3 +16,54 @@ $(".gnb li").mouseleave(function(){
 $(".box>.top_icon>dl>dd:last").click(function(){
     $(".menuAll").slideToggle();
 });
+
+let num = 0;
+let width = $(".imgbox>div").outerWidth();
+console.log(width);
+ $(".imgbox>div:first").clone(true).insertAfter(".imgbox>div:last");
+setInterval(function(){
+    num++;
+    $(".imgbox").stop().animate({left: num*-width}, 500, function(){
+        if(num > 2){
+            num = 0;
+            $(".imgbox").css("left", 0);
+        }
+    });
+},2000);
+
+let table= "";
+$(".box>.top_icon>dl>dd").eq(2).click(function(){
+    console.log("asdfasdfasdf");
+    $("#popup").fadeIn();
+
+    $.ajax({
+        url:"js/menu.json",
+        dataType: "json",
+        success: function(data){
+            if(table != ""){
+                return;
+            }
+            table = $("<table>");
+            for(let i in data){
+                let $id = data[i].id;
+                let $name = data[i].name;
+                let $pay = data[i].pay;
+                let $sale = data[i].sale;
+
+                let row = $("<tr>").append(
+                    $("<td>").text($id),
+                    $("<td>").text($name),
+                    $("<td>").text($pay),
+                    $("<td>").text($sale),
+                );
+                table.append(row);
+            }
+            $(".modal>.list").append(table);
+        }
+    });
+});
+$(document).mouseup(function(e){
+    if ($("#popup").has(e.target).length === 0){
+        $("#popup").fadeOut();
+    }
+});
